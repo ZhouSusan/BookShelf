ac1b2ff499eb24230d278925a8f2fd1e3347eedc
ZhouSusan <szhou089@gmail.com>
2022-11-14 16:39:15 -0800

    added class BookCollections, where you can add a book to the arrayList and print out each book details

--- src/main/java/org/example/BookCollections.java
+++ src/main/java/org/example/BookCollections.java
@@ -1,4 +1,30 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookCollections {
+    private Book _book;
+    private List<Book> bookList;
+
+    public BookCollections() {
+        bookList = new ArrayList<Book>();
+    }
+
+    public void setBook(Book book) {
+        this._book = book;
+    }
+
+    public Book getBook() {
+        return this._book;
+    }
+
+    public void addBook(Book book) {
+        bookList.add(book);
+    }
+
+    public void printAllBooksInfo() {
+        for (Book b: bookList) {
+            System.out.format("\n***************\n%s", b.toString());
+        }
+    }
 }
--- src/main/java/org/example/BookShelf.java
+++ src/main/java/org/example/BookShelf.java
@@ -3,13 +3,24 @@ package org.example;
 //BookShelf contains the main method which will be our driver class
 public class BookShelf {
     public static void main(String[] args) {
-        Book book = new Book("Choko Bunny", "Fred", "Tweet and Sweeter Publishing House",
+        Book book1 = new Book("Choko Bunny", "Fred", "Tweet and Sweeter Publishing House",
                 "555-777-88", 1989);
-        System.out.println(book.toString());
-        System.out.println("*********************");
-        book.setTitle("Jurassic Park");
-        book.setAuthor("Fred Tutorial");
-        book.setCopyRight(1970);
-        System.out.println(book.toString());
+        Book book2 = new Book("Avengers", "Steve Jobs", "Tweet and Sweeter Publishing House",
+                "555-777-99", 1992);
+        Book book3 = new Book("Star Wars", "Luke Skyler", "May the Force be with you Publishing House",
+                "575-478-42", 1985);
+//        System.out.println(book.toString());
+//        System.out.println("*********************");
+//        book.setTitle("Jurassic Park");
+//        book.setAuthor("Fred Tutorial");
+//        book.setCopyRight(1970);
+//        System.out.println(book.toString());
+
+        BookCollections myBookCollection = new BookCollections();
+        myBookCollection.addBook(book1);
+        myBookCollection.addBook(book2);
+        myBookCollection.addBook(book3);
+
+        myBookCollection.printAllBooksInfo();
     }
 }
